c76bfd02253c4c8a17c1425ed2f07ac32a9d7052
TranDuongHoan <133374478+TranDuongHoan@users.noreply.github.com>
2023-06-01 16:36:14 +0700

    update

---
+++ production/tranduonghoan/com/tranduonghoan/app/String.java
@@ -0,0 +1,18 @@
+package com.tranduonghoan.app;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tên:");
+        String name = sc.nextLine();
+
+        System.out.println("Nhập tuổi:");
+        int age = sc.nextInt();
+
+        System.out.println(name + "có tuổi là" + age + "có giới tính" + sex);
+
+    }
+}
